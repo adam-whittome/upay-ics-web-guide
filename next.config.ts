dbf726461237c7ef4a5205ee4e7f4405979d42b6
Adam Whittome <aw2095@cam.ac.uk>
2026-02-03 18:07:45 +0000

    Changed for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  assetPrefix: isProd ? '/upay-ics-web-guide/' : '',
+  basePath: isProd ? '/upay-ics-web-guide' : '',
+  output: 'export'
 };
 
 export default nextConfig;

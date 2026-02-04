acd56a8acd3d774dcb89dd19a47629df1c88777c
Adam Whittome <aw2095@cam.ac.uk>
2026-02-04 00:10:44 +0000

    Refactored CopyBox

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   assetPrefix: isProd ? '/upay-ics-web-guide/' : '',
   basePath: isProd ? '/upay-ics-web-guide' : '',
--- src/components/molecules/CopyBox.tsx
+++ src/components/molecules/CopyBox.tsx
@@ -1,17 +1,16 @@
-import { LuCopy } from "react-icons/lu";
+import { ClipboardIcon } from "@heroicons/react/24/outline";
 
 import Text from "../atoms/Text";
-import IconButton from "./IconButton";
 
 export default function CopyBox(props: { text: string }) {
   return <div className="flex flex-row p-1 px-2 bg-background-secondary rounded-md items-center gap-2">
     <div className="flex-1 break-all">
       <Text>{props.text}</Text>
     </div>
-    <div className="flex-none">
-      <IconButton onClick={() => navigator.clipboard.writeText(props.text)}>
-        <LuCopy />
-      </IconButton>
-    </div>
+    <button className="flex-none cursor-pointer"
+      onClick={() => navigator.clipboard.writeText(props.text)}
+    >
+      <ClipboardIcon className="size-5 stroke-foreground transition hover:stroke-foreground-secondary" />
+    </button>
   </div>
 }
